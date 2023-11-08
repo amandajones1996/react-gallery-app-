f678115123b41aa6e0c56edd76e216689c5d0bff
amandajones1996 <jonesamanda996@gmail.com>
2023-11-07 19:08:47 -0500

    siting resouces

--- react-gallery-app/src/components/PhotoList.jsx
+++ react-gallery-app/src/components/PhotoList.jsx
@@ -6,7 +6,7 @@ import { useParams } from "react-router-dom";
 const PhotoList = ({ photos, updateUrlWithQuery, title }) => {
     const { queryTag } = useParams();
 
-    // error trying to render when updating state without this "badstate?"
+    // error trying to render when updating state without this. Resources used: https://flaviocopes.com/react-update-while-rendering-different-component/ + https://stackoverflow.com/questions/62336340/cannot-update-a-component-while-rendering-a-different-component-warning
     useEffect(() => {
         updateUrlWithQuery(queryTag)
     }, [updateUrlWithQuery, queryTag])
